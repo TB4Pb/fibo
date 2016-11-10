14ee2073ec7a082f3f58619a6134f2fc72c9b21b
Pavel <pashuk06@mail.ru>
2016-11-10 13:02:49 +0000

    fibo

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// var num = +prompt("Enter Number");
+// function fibonacci(n) {
+//   var a=0;
+//   var b=1;
+//   var c;
+//     for ( n; n>0; n--) {
+//         c = b;
+//         b = b + a;
+//         a = c; 
+//     }
+//     return a;
+// }
+// var result = fibonacci(num);
+// document.write(result); 
+
+
+var num = +prompt("Enter Number");
+var result = fibonacci(num);
+document.write(result);
+
+function fibonacci(num) {
+  return num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2);
+}
